49251b11e913361e2abfaf8386e1ce3757da8ec5
Ryan Sparks <rsparks@club-os.com>
2023-04-12 15:02:11 -0500

    Changing to webpack

---
+++ src/index.js
@@ -0,0 +1,106 @@
+export { default as Button } from './components/atoms/button'
+export { default as Modal } from './components/modal'
+export { default as Select } from './components/atoms/select'
+export { default as Alert } from './components/atoms/alert'
+export { default as Icon } from './components/atoms/icon'
+export { default as ToggleButton } from './components/atoms/toggleButton'
+export { default as ToggleCheck } from './components/atoms/toggleCheck'
+export { default as CheckBox } from './components/atoms/checkBox'
+export { default as TimePicker } from './components/atoms/timePicker'
+export { default as Tabs } from './components/tabs'
+export { default as Tooltip } from './components/tooltip'
+export { default as Offcanvas } from './components/offcanvas'
+export { default as LocationSelect } from './components/locationSelect'
+export { default as Editor } from './components/editor'
+
+export {
+    default as Paper,
+    PaperProps,
+} from './components/UI-Components/Paper/Paper';
+
+export {
+    default as LocationMultiSelectFieldGroup,
+    LocationMultiSelectFieldGroupProps
+} from './components/UI-Components/LocationMultiSelectFieldGroup/LocationMultiSelectFieldGroup';
+
+export {
+    default as LocationMultiSelectFieldGroupButton,
+    LocationMultiSelectFieldGroupButtonProps
+} from './components/UI-Components/LocationMultiSelectFieldGroupButton/LocationMultiSelectFieldGroupButton';
+
+export {
+    default as SelectLocationsWithSmsTooltip
+} from './components/UI-Components/Tooltip/IconTooltip/IconTooltip';
+
+export {
+    default as TextInput,
+    TextInputProps
+} from './components/UI-Components/TextInput/TextInput';
+
+export {
+    default as TextField,
+} from './components/UI-Components/TextField/TextField';
+
+export { TextFieldProps } from '@material-ui/core/TextField';
+
+export {
+    default as ButtonWithIcon,
+    ButtonWithIconProps
+} from './components/UI-Components/Buttons/ButtonWithIcon/ButtonWithIcon';
+
+export {
+    default as SimplePopover,
+    SimplePopoverProps
+} from './components/UI-Components/SimplePopover/SimplePopover';
+
+export {
+    default as IconTooltip,
+    IconTooltipProps
+} from './components/UI-Components/Tooltip/IconTooltip/IconTooltip';
+
+export {
+    default as DateRangeFieldset,
+    DateRangeFieldsetProps
+} from './components/UI-Components/DateRangeFieldset/DateRangeFieldset';
+
+export {
+    default as UISelect,
+    SelectProps
+} from './components/UI-Components/Select/Select';
+
+export {
+    default as Snackbar,
+    SnackbarProps
+} from './components/UI-Components/Snackbar/Snackbar';
+
+export {
+    default as SingleSearchSelect,
+    SingleSearchSelectProps
+} from './components/UI-Components/SingleSearchSelect/SingleSearchSelect';
+
+export {
+    default as MultiSelectFieldGroup,
+    MultiSelectFieldGroupProps,
+    MultiSelectRadioInputProps,
+} from './components/UI-Components/MultiSelectFieldGroup/MultiSelectFieldGroup';
+
+export {
+    default as MultiSelectSearchList,
+    MultiSelectOptionData,
+    MultiSelectGroupValueWrapper
+} from './components/UI-Components/MultiSelectFieldGroup/MultiSelectSearchList/MultiSelectSearchList';
+
+export {
+    default as Accordions,
+    AccordionsProps,
+    Accordion,
+} from './components/UI-Components/Accordions/Accordions';
+
+export {
+    getRegionsOrLocationsSelectedLabel,
+    RenderButtonLabelParams
+} from './components/UI-Components/LocationMultiSelectFieldGroup/locationMultiSelectFieldGroupHelper';
+
+export { default as clubOsTheme } from './components/UI-Components/themes/clubOS';
+export { default as goldsTheme } from './components/UI-Components/themes/golds';
+
